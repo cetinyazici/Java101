3e301229431d9449677375103d6e2fa5e209c827
Çetin Yazıcı <56675464+cetinyazici@users.noreply.github.com>
2024-01-13 18:41:04 +0300

    Dizideki Elemanla Karşılaştırma
    Dizideki elemanların girilen sayıdan küçük en yakın sayıyı ve en büyük en yakın sayıyı bulan program.

---
+++ DizidekiMaksimumMinimumDeğerleriBulma/src/dizidekimaksimumminimumdeğerleribulma/Main.java
@@ -0,0 +1,52 @@
+package dizidekimaksimumminimumdeğerleribulma;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        //Diziyi Tanımlama 
+        int[] array = {15,12,788,1,-1,-778,2,0};
+        
+        //kullanıcıdan sayı alma 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Girilen Sayı: ");
+        int number = input.nextInt();
+        
+        
+        //En yakın sayıları bulma
+        int enKucukYakın = bulEnKucukYakinSayi(array, number);
+        int enBuyukkYakın = bulEnBuyukYakinSayi(array, number);
+        
+        //Sonuçları Ekrana Yazdırma kısmı 
+        System.out.println("Girilen sayı: "+number);
+        System.out.println("Girilen sayıdan buyuk en yakın sayı: "+enKucukYakın);
+        System.out.println("Girilen sayıdan kucuk en yakın sayı: "+enBuyukkYakın);
+         
+    }
+    
+    private static int bulEnKucukYakinSayi(int[] array, int number)
+    {
+        Arrays.sort(array);
+        for(int i=array.length-1;i>=0;i--)
+        {
+            if(array[i]<number)
+            {
+                return array[i];
+            }
+        }
+        return -1;
+    }
+    
+    private static int bulEnBuyukYakinSayi(int[] dizi, int sayi) {
+        Arrays.sort(dizi);
+        for (int i = 0; i < dizi.length; i++) {
+            if (dizi[i] > sayi) {
+                return dizi[i];
+            }
+        }
+        return -1; 
+    }
+    
+}
